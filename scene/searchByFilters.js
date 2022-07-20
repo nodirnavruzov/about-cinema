8816dcf1f6d3cabac9cfb40ad4b41e1c5b76d0d1
Nodir Navruzov <spy.nowin@gmail.com>
2022-07-20 20:56:39 +0500

    finish genres

--- scene/searchByFilters.js
+++ scene/searchByFilters.js
@@ -19,6 +19,11 @@ const searchByFilter = new Scenes.BaseScene('searchByFilterScene')
 
 searchByFilter.enter(async (ctx) => {
   await filterMenu(ctx)
+  ctx.session.genre = {
+    page: 1,
+    genreId: null,
+    totalPages: 0
+  }
   return searchByFilter 
 })
 
@@ -63,7 +68,8 @@ searchByFilter.action(/^\d+$/ , async (ctx) => {
   try {
     await ctx.answerCbQuery()
     const genreId = ctx.match[0]
-    await getMoviesByGenre(ctx, genreId)
+    ctx.session.genre.genreId = genreId
+    await getMoviesByGenre(ctx)
   } catch (error) {
     console.log('error', error)
     return await ctx.reply(`${ctx.i18n.t('whoops')}`)
@@ -138,6 +144,18 @@ searchByFilter.action(/(wl_.+)/, async (ctx) => {
   }
 })
 
+
+// more movies
+searchByFilter.action(/(id_.+)/, async (ctx) => {
+  try {
+    ctx.session.genre.page++
+    getMoviesByGenre(ctx)
+  } catch (error) {
+    console.log('error', error)
+    return await ctx.reply(`${ctx.i18n.t('whoops')}`)
+  }
+})
+
 // get trailer
 searchByFilter.action(/^(?!wl_).*$/, async (ctx) => {
   try {
@@ -150,59 +168,70 @@ searchByFilter.action(/^(?!wl_).*$/, async (ctx) => {
     return await ctx.reply(`${ctx.i18n.t('whoops')}`)
   }
 })
-
-async function getMoviesByGenre(ctx, genreId) {
+async function getMoviesByGenre(ctx) {
   try {
+    const page = ctx.session.genre.page
+    const genreId = ctx.session.genre.genreId
+    console.log('GENRE ===>', ctx.session.genre)
     const options = {
       method: 'GET',
       headers: {
         'X-API-KEY': process.env.API_KEY_KINOPOISK,
         'Content-Type': 'application/json',
       },
-      url: `https://kinopoiskapiunofficial.tech/api/v2.2/films?genres=${genreId}&order=RATING&type=FILM&ratingFrom=5&ratingTo=10&yearFrom=1000&yearTo=3000&page=1`
+      url: `https://kinopoiskapiunofficial.tech/api/v2.2/films?genres=${genreId}&order=RATING&type=FILM&ratingFrom=5&ratingTo=10&yearFrom=1000&yearTo=3000&page=${page}`
     };
     const { data } = await axios(options)
     const parsedData = toHtml(data.items)
-    for (let i = 0; i < parsedData.length; i++) {
-      const movie = parsedData[i];
-      await sendMovies(ctx, movie)
-    }
+    ctx.session.genre.totalPages = data.totalPages
+    await sendMovies(ctx, parsedData)
   } catch (error){
     console.log(error)
     return await ctx.reply(`${ctx.i18n.t('whoops')}`)
   }
 }
 
-async function sendMovies(ctx, movie) {
+async function sendMovies(ctx, movies) {
   try {
-    if (movie.title && movie.title.length > 21) {
-      movie.title = movie.title.slice(0, 21)
-    }
-    return await ctx.replyWithPhoto({url: movie.poster}, { caption: movie.html, parse_mode: 'HTML',
-      ...Markup.inlineKeyboard(
-        [
+    for (let i = 0; i < movies.length; i++) {
+      const movie = movies[i];
+      let buttons = [] 
+      if (movie.title && movie.title.length > 21) {
+        movie.title = movie.title.slice(0, 21)
+      }
+      if (movies.length === i + 1 && ctx.session.genre.totalPages !== ctx.session.genre.page) {
+        buttons = [
           [
-              Markup.button.callback(`${ctx.i18n.t('trailer')}`, `${movie.title}`), 
-              Markup.button.callback(`${ctx.i18n.t('add_watchlist')}`, `wl_${movie.film_id}`), 
+            Markup.button.callback(`${ctx.i18n.t('trailer')}`, `${movie.title}`), 
+            Markup.button.callback(`${ctx.i18n.t('add_watchlist')}`, `wl_${movie.film_id}`), 
+          ],
+          [
+            Markup.button.callback(`${ctx.i18n.t('more')}`, `id_${movie.film_id}`), 
           ]
         ]
-      )
-    })
+      } else {
+        buttons = [
+          [
+            Markup.button.callback(`${ctx.i18n.t('trailer')}`, `${movie.title}`), 
+            Markup.button.callback(`${ctx.i18n.t('add_watchlist')}`, `wl_${movie.film_id}`), 
+          ]
+        ]
+      }
+
+      await ctx.replyWithPhoto({url: movie.poster}, { caption: movie.html, parse_mode: 'HTML',
+        ...Markup.inlineKeyboard(
+          buttons
+        )
+      })
+    }
+
   } catch(error) {
     console.log(error)
     return await ctx.reply(`${ctx.i18n.t('whoops')}`)
   }
 }
 
-
-
-
 module.exports = searchByFilter
-
-
-
-
-
 
 // https://kinopoiskapiunofficial.tech/api/v2.2/films?genres=1&genres=4&genres=0&order=RATING&type=ALL&ratingFrom=0&ratingTo=10&yearFrom=1000&yearTo=3000&page=1'
 // https://kinopoiskapiunofficial.tech/api/v2.2/films?genres=1&order=RATING&type=FILM&ratingFrom=7&ratingTo=10&yearFrom=1000&yearTo=3000&page=1
